bb1585e3e3f93c3d2c090736bf654f50d4c8bb55
Richard Karlsson <richard.karlsson@iag.com.au>
2019-04-02 15:35:56 +1100

    Making a token change to test Tealium integration

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 // Some publically available script that doesn't really do anything...
 (function() {
   var one = "I am a great string of text!";
-  console.log("What did you say? ", one)
+  // console.log("What did you say? ", one)
   function test() {
     var two = "hello";
   }
+  var changeToGitRepo = "This change was made to the GitHub repo on the 2nd of April 2019.";
 })();
